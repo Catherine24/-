12de1077bd4a92f38dc02596a3e4255675329545
Catherine24 <katerina00.00@bk.ru>
2020-09-06 16:40:55 +0300

    test

--- forllop/main.go
+++ forllop/main.go
@@ -4,39 +4,22 @@ import "fmt"
 
 func main() {
 	intArr := []int{1, -4, -2, 3, 9, 5, 7, 1, 0, -3, -3, 5}
-	sum := 0
-	for _, value := range intArr {
-		sum +=value
-		if value <= sum/len(intArr) {
-			fmt.Println(value)
+
+	for i := 0; i < len(intArr); i++ {
+		for j := len(intArr) - 1; j > i; j-- {
+			if intArr[j-1] > intArr[j] {
+				tmp := intArr[j-1]
+				intArr[j-1] = intArr[j]
+				intArr[j] = tmp
+			}
 		}
 	}
+	fmt.Println(intArr[0], intArr[1])
 }
 
-//func sort() {
-//	intArr := []int{1,4,2,3,9,5,7,1,0,3,3,5}
+//
+//
 
-//for i := 0; i < len(intArr); i++ {
-//	for j := len(intArr) - 1; j > i; j-- {
-//		if intArr[j-1] > intArr[j] {
-//			tmp := intArr[j-1]
-//			intArr[j-1] = intArr[j]
-//			intArr[j] = tmp
-//
-//		}
-//	}
-//	//}
-//
-//
-//	for i := 0; i < len(intArr) - 1; i++ {
-//		for j := i+1; j < len(intArr); j++ {
-//			if intArr[j] < intArr[i] {
-//				tmp := intArr[i]
-//				intArr[i] = intArr[j]
-//				intArr[j] = tmp
-//			}
-//		}
-//	}
 //
 //
 //	for i := 0; i < len(intArr); i++ {

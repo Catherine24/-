e0ba59cc8dd72d1c5073cce98f404fcb3e5b66d7
Catherine24 <katerina00.00@bk.ru>
2020-09-06 18:57:05 +0300

    reset slice

--- forllop/main.go
+++ forllop/main.go
@@ -4,16 +4,18 @@ import "fmt"
 
 func main() {
 	intArr := []int{1, -4, -2, 3, 9, 5, 7, 1, 0, -3, -3, 5}
-
+	n := make([]int, 0 , cap(intArr))
+	a := 1
+	b := 5
 	for i := 0; i < len(intArr); i++ {
-		for j := len(intArr) - 1; j > i; j-- {
-			if intArr[j-1] > intArr[j] {
-				tmp := intArr[j-1]
-				intArr[j-1] = intArr[j]
-				intArr[j] = tmp
+		tmp := intArr[i]
+		if tmp >= a {
+			intArr[i] = tmp
+			if tmp <= b{
+				intArr[i] = tmp
+				n = append(n,tmp)
 			}
 		}
 	}
-	fmt.Println(intArr[0], intArr[1])
+	fmt.Println(n)
 }
-

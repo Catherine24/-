43b23b410015a02fccb75c22c46bd9b32c6ac915
Catherine24 <katerina00.00@bk.ru>
2020-09-08 01:22:39 +0300

    negativesum

---
+++ lab3/msum.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	intArr := []int{1, -4, -2, 3, 9, 5, 7, 1, 0, -3, -3, 5}
+	sum := 0
+	negative := 0
+	for i := 0; i < len(intArr); i++ {
+		tmp := intArr[i]
+
+		if tmp > 0 && negative >=0{
+			continue // пропускаем текущее
+		} else if tmp < 0 {
+			negative = tmp
+		}
+
+		if tmp <= 0 {
+			tmp= tmp * -1
+		}
+
+		sum += tmp
+	}
+	fmt.Println(sum)
+}
